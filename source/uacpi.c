23070188450ed78fa5bf2736bb1fc8eed707e0a2
Daniil Tatianin <99danilt@gmail.com>
2025-03-20 22:57:07 +0300

    uacpi: handle failure to initialize registers correctly
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- source/uacpi.c
+++ source/uacpi.c
@@ -334,7 +334,7 @@ uacpi_status uacpi_initialize(uacpi_u64 flags)
 
     ret = uacpi_initialize_registers();
     if (uacpi_unlikely_error(ret))
-        return ret;
+        goto out_fatal_error;
 
     ret = uacpi_initialize_events_early();
     if (uacpi_unlikely_error(ret))
