22895b49e372303e1fb8338cead78534ed44b966
Daniil Tatianin <99danilt@gmail.com>
2025-04-22 23:04:24 +0300

    resources: return UACPI_POISITIVE_DECODE and add a comment
    This is part of the public API so removing it warrants a major version
    bump, which we don't want to do yet.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- include/uacpi/resources.h
+++ include/uacpi/resources.h
@@ -214,6 +214,12 @@ typedef struct uacpi_resource_fixed_io {
 #define UACPI_POSITIVE_DECODE 0
 #define UACPI_SUBTRACTIVE_DECODE 1
 
+/*
+ * DO NOT USE! SLATED FOR REMOVAL AT 3.0
+ * See the version without the typo above (UACPI_POSITIVE_DECODE)
+ */
+#define UACPI_POISITIVE_DECODE 0
+
 // fixed_min_address & fixed_max_address
 #define UACPI_ADDRESS_NOT_FIXED 0
 #define UACPI_ADDRESS_FIXED 1
